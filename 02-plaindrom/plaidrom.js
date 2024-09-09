9f423eca5a2b1dd6e26df1615d4627931cd815d3
Masoumeh Ahmadi <maasoumeh.ahmadi@email.kntu.ac.ir>
2024-09-09 15:17:07 +0330

    Solve exercise two

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,11 @@
 // write your func here
+function palinrom(str){
+  let revrs=str.split("").reverse().join("");
+  if(str===revrs){
+    return true;
+  }else{
+    return false;
+  }
+}
+let inpstr =prompt("enter your string :");
+console.log(palinrom(inpstr));
