344122c0386581f3efdba0cb8c720cf188ee7727
Masoumeh Ahmadi <maasoumeh.ahmadi@email.kntu.ac.ir>
2024-09-09 21:39:29 +0330

    Solve exercise four

--- 04-FizzBuzz/fizzBuzz.js
+++ 04-FizzBuzz/fizzBuzz.js
@@ -1,1 +1,15 @@
 // write your func here
+for(i=1 ; i<=100 ;i++){
+  if( i%3===0 || i%5===0){
+    if(i%3===0 && i%5!=0){
+      console.log("Fizz");
+    }
+    if(i%5===0 && i%3!=0){
+      console.log("Buzz");
+    }
+    if(i%3===0 && i%5===0){
+      console.log("Fizz Buzz");
+    }
+  }else
+  console.log(i);
+}
