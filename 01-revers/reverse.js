2a5f03bff1a24ff4800c9231989a832f8d5b3ff4
Masoumeh Ahmadi <maasoumeh.ahmadi@email.kntu.ac.ir>
2024-09-09 14:53:56 +0330

    Solve exercise one

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,13 @@
 // write your func here
+function revers(str){
+  let result="";
+  let rvrs=str.split("").reverse();
+  for(i=0; i<rvrs.length; i++){
+    result=result.concat(rvrs[i]);
+  }
+  return result;
+}
+
+let inputStr =prompt("enter your string :");
+
+console.log(revers(inputStr));
