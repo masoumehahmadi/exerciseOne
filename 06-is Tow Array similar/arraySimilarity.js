0002d37ab0c72d7cd900b5dc6f887faaad6435d2
Masoumeh Ahmadi <maasoumeh.ahmadi@email.kntu.ac.ir>
2024-09-09 21:41:23 +0330

    Solve exercise six

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,17 @@
 // write your func here
+function equalArray(arr1,arr2){
+  if(arr1.length == arr2.length){
+    for(let i of arr1){
+      if(arr1[i]===arr2[i]){
+        return true;
+      }else
+      return false;
+    }
+  }else{
+    return false;
+  }
+}
+let arr1=[1,2,3];
+let arr2=[1,2,3];
+
+console.log(equalArray(arr1,arr2))
