05dce784395b1bd24f2dfbf5ea94d271f8b814ad
Masoumeh Ahmadi <maasoumeh.ahmadi@email.kntu.ac.ir>
2024-09-09 21:40:26 +0330

    Solve exercise five

--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,8 @@
 // write your func here
+function maximum(arr){
+  return Math.max(...arr);
+}
+let num =prompt("enter your number with spaces between them :").split(" ");
+let numArray=num.map(Number);
+console.log(numArray)
+console.log(maximum(numArray));
